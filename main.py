0ac6b3cc2a5619fefc75f1535b9eb8072942e4cb
kshitij7704 <kashyapkshitij7704@gmail.com>
2024-06-26 11:24:27 +0530

    Handled validation for home route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
@@ -7,9 +7,45 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "session_key"
 socketio = SocketIO(app)
 
+rooms = {}
+
+def generate_room_code(length):
+    while True:
+        code = ""
+        for i in range(length):
+            code += random.choice(ascii_uppercase)
+        if code not in rooms:
+            break
+    return code
+
 @app.route("/", methods = ["GET", "POST"])
 def home():
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="Please enter a name", code = code, name = name)
+        if join != False and not code:
+            return render_template("home.html", error="Please enter a room code to join", code = code, name = name)
+        room = code
+        if create != False:
+            room = generate_room_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template("home.html", error="Room code does not exist", code = code, name = name)
+        
+        session["name"] = name
+        session["room"] = room
+        return redirect(url_for("room"))
+ 
     return render_template("home.html")
+    
+@app.route("/room")
+def room():
+    return render_template("room.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug = True)
