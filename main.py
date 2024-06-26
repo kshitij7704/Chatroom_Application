34317704581294e9107c5233ce88ab8e72017daa
kshitij7704 <kashyapkshitij7704@gmail.com>
2024-06-26 19:33:18 +0530

    Completed sending message

--- main.py
+++ main.py
@@ -66,6 +66,32 @@ def connect(auth):
     rooms[room]["members"] += 1
     print(f"{name} joined room {room}")
 
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to = room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+    
+    send({"name": name, "message": "has left the room"}, to = room)
+    print(f"{name} has left the room {room}")
 
 if __name__ == "__main__":
     socketio.run(app, debug = True)
